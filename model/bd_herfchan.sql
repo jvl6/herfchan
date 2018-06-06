aafdb27d047c561cc582d8932b0e1cfabfa70ab9
jvlee6 <jvergaralee@gmail.com>
2018-06-06 10:08:08 -0400

    Creadas un par de tablas en la base de datos.

--- model/bd_herfchan.sql
+++ model/bd_herfchan.sql
@@ -6,14 +6,33 @@ CREATE TABLE IF NOT EXISTS board(
     nombre VARCHAR(200)
 ); -- DROP TABLE board;
 
+CREATE TABLE IF NOT EXISTS moderador(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(200),
+    fk_board LONG REFERENCES board (id)
+); -- DROP TABLE moderador;
+
+CREATE TABLE IF NOT EXISTS ban(
+	ip VARBINARY(200) UNIQUE PRIMARY KEY,
+    reason VARCHAR(200),
+    fk_moderador INT REFERENCES moderador (id)
+); -- DROP TABLE ban;
+
+CREATE TABLE IF NOT EXISTS postUser(
+	id LONG AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(200)
+); -- DROP TABLE postUser;
+
 CREATE TABLE IF NOT EXISTS thread(
 	id LONG AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(200),
-    fk_board INT REFERENCES board (id)
+    fk_board INT REFERENCES board (id),
+    fk_user INT REFERENCES postUser (id)
 ); -- DROP TABLE thread;
 
 CREATE TABLE IF NOT EXISTS post(
 	id LONG AUTO_INCREMENT PRIMARY KEY,
     mensaje VARCHAR(60000),
-    fk_thread INT REFERENCES thread (id)
+    fk_thread INT REFERENCES thread (id),
+    fk_user INT REFERENCES postUser (id)
 ); -- DROP TABLE post;
