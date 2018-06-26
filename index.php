c668ef7426f5d41eab50fb76d037b93d22ac89ef
janjell <jorge.anjel3@gmail.com>
2018-06-25 20:27:07 -0400

    Mejoras en index + uso de herfito.jpg.

--- index.php
+++ index.php
@@ -35,6 +35,10 @@
 
     <br>
 
+        <div class="container text-right">
+            <img src="res/herfito.jpg" alt="" class="img-thumbnail rounded float-right" style="width: 50%;">
+        </div>
+
     <div class="container">
         <div id="accordion">
             <div class="card" style="width: 18rem;">
@@ -65,7 +69,7 @@
         $stats = $d->verStats("board");
 
         echo "<div class='container'>";
-        echo "<div class='card' style='width: 40rem;'>";
+        echo "<div class='card' style='width: 18rem;'>";
             echo "<div class='card-header'>";
             echo "<h5 class='card-title text-center'>Estadísticas</h5>";
             echo "</div>";
@@ -110,6 +114,8 @@
     ?>
 
     <br>
+
+    <center><p>Copyright © 2018 HerfChan community support LLC. All rights reserved. </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
